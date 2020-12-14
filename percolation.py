2b1ea076e812a4b4df52931d7f768858d4d23b26
Luis Ramirez Mendoza <luism.ramirez23@gmail.com>
2020-12-14 11:19:45 -0800

    Final Version

--- percolation.py
+++ percolation.py
@@ -1,27 +1,3 @@
-class Vertex:
-	# `index` is a unique integer identifier, `color` is an integer in [-1, 0, 1].
-	# Silver vertices have color=0, and teal vertices have color=1.
-	# Unmarked vertices have color=-1.
-	def __init__(self, index, color=-1):
-		self.index = index
-		self.color = color
-
-class Edge:
-	# `a` and `b` are Vertex objects corresponding to the endpoints of this edge.
-	def __init__(self, a, b):
-		self.a = a
-		self.b = b
-
-class Graph:
-	# `vertices` and `edges` are iterables of Vertex and Edge objects respectively
-	# Internally, we store these as set()s on the graph class.
-	def __init__(self, vertices, edges):
-		self.V = set(vertices)
-		self.E = set(edges)
-
-
-#MY STUFF
-
 class PercolationPlayer:
 	# `graph` is an instance of a Graph, `player` is an integer (0 or 1).
 	# Should return a vertex `v` from graph.V where v.color == -1
@@ -61,11 +37,3 @@ class PercolationPlayer:
             if len(PercolationPlayer.getAttachedEdges(graph, i)) > len(PercolationPlayer.getAttachedEdges(graph,greatest)) and i.color == active_player:
                 greatest = i
         return greatest
-
-
-# Feel free to put any personal driver code here.
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
